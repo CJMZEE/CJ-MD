15d10bc7a2dcb6411509ba250aead86986fa3aed
Carl William 🕵 <cwilly403@gmail.com>
2024-11-25 07:53:57 +0300

    ‌

---
+++ config.js
@@ -0,0 +1,44 @@
+
+
+
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('config.env'))
+    require('dotenv').config({ path: __dirname + '/config.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || '',
+    PREFIXE: process.env.PREFIX || ".",
+    OWNER_NAME: process.env.OWNER_NAME || "Carl William",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " Carl william",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
+    // AUTO_REACT: process.env.AUTO_REACT || 'yes',
+   //  AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'yes',
+    BOT : process.env.BOT_NAME || 'CYBERION-SPARK-X',
+    URL : process.env.BOT_MENU_LINKS || 'https://i.imgur.com/ntJX1Pf.jpeg',
+    MODE: process.env.PUBLIC_MODE || "yes",
+    PM_PERMIT: process.env.PM_PERMIT || 'yes',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ETAT : process.env.PRESENCE || '',
+    CHATBOT : process.env.PM_CHATBOT || 'no',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+                  AUTO_READ : process.env.AUTO_READ || 'yes',
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+   
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
